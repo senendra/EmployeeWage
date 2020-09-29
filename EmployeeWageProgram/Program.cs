9f68694d3cb08c5fce09f3dd3bf172ceb720af15
Senendra <senendra.007@gmail.com>
2020-09-29 14:03:30 +0530

    [Senendra] Refactor: Updating with part time employee

--- EmployeeWageProgram/Program.cs
+++ EmployeeWageProgram/Program.cs
@@ -5,6 +5,7 @@ namespace EmployeeWageProgram
     {
         //Constant
         public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
@@ -14,11 +15,16 @@ namespace EmployeeWageProgram
             //Variable
             int empHrs = 0;
             int empWage = 0;
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Present Full Time");
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Present Part Time");
+                empHrs = 4;
             }
             else
             {
@@ -26,7 +32,7 @@ namespace EmployeeWageProgram
                 empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: " + empWage);
+            Console.WriteLine("Employee Wage: " + empWage);
         }
     }
 }
