5cd5ab530bcb576036b4469357546fedef2645d7
Senendra <senendra.007@gmail.com>
2020-09-29 13:40:47 +0530

    [Senendra] Add: Welcome Message

---
+++ EmployeeWageProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EmployeeWageProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welocome to Employee Wage Computation Program");
+        }
+    }
+}
