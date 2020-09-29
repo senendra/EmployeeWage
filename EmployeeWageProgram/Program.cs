58db183219e0e79114fa525433e2b45a206a9eb7
Senendra <senendra.007@gmail.com>
2020-09-29 13:56:04 +0530

    [Senendra] Refactor: Displaying Daily Wage

--- EmployeeWageProgram/Program.cs
+++ EmployeeWageProgram/Program.cs
@@ -5,20 +5,28 @@ namespace EmployeeWageProgram
     {
         //Constant
         public const int IS_FULL_TIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
             //Welcome Message
             Console.WriteLine("Welocome to Employee Wage Computation Program");
             Random random = new Random();
+            //Variable
+            int empHrs = 0;
+            int empWage = 0;
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 }
