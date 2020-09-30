1d5ad55d3acec6c13797cd745d8cc4c4a0a2dc66
Senendra <senendra.007@gmail.com>
2020-09-30 11:08:51 +0530

    [Senendra] Refactor: Saving the Total Employee Wage of each company

---
+++ EmployeeWageProgram/EmpWageBuilderObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace EmployeeWageProgram
+{
+    class EmpWageBuilderObject
+    {
+        //Constant
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        private string company;
+        private int EMP_RATE_PER_HOUR;
+        private int MAX_WORKING_DAYS;
+        private int MAX_HOURS;
+        private int totalEmpWage = 0;
+        public EmpWageBuilderObject(String company, int EMP_RATE_PER_HOUR, int MAX_WORKING_DAYS, int MAX_HOURS)
+        {
+            this.company = company;
+            this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+            this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
+            this.MAX_HOURS = MAX_HOURS;
+        }
+        public void computeEmployeeWage()
+        {
+            Random random = new Random();
+            //Variable
+            int empHrs = 0;
+            int empTotalHour = 0;
+            int day = 0;
+            while ((day < this.MAX_WORKING_DAYS) && (empTotalHour < this.MAX_HOURS))
+            {
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empTotalHour += empHrs;
+                day++;
+            }
+            totalEmpWage = empTotalHour * this.EMP_RATE_PER_HOUR;
+        }
+        public String toString()
+        {
+           return "Total Employee Wage for company " + this.company + " is " + this.totalEmpWage;
+        }
+    }
+}
--- EmployeeWageProgram/Program.cs
+++ EmployeeWageProgram/Program.cs
@@ -3,45 +3,16 @@ namespace EmployeeWageProgram
 {
     class Program
     {
-        //Constant
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-        public static int computeEmployeeWage(String company, int EMP_RATE_PER_HOUR, int MAX_WORKING_DAYS, int MAX_HOURS)
-        {
-            Random random = new Random();
-            //Variable
-            int empHrs = 0;
-            int empTotalHour = 0;
-            int day = 0;
-            int totalEmpWage = 0;
-            while ((day < MAX_WORKING_DAYS) && (empTotalHour < MAX_HOURS))
-            {
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                   case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                empTotalHour += empHrs;
-                day++;
-            }
-            totalEmpWage = empTotalHour * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Employee Wage for company "+company+" is " + totalEmpWage);
-            return totalEmpWage;
-        } 
         static void Main(string[] args)
         {
             //Welcome Message
             Console.WriteLine("Welocome to Employee Wage Computation Program");
-            computeEmployeeWage("Dmart",20,20,100);
-            computeEmployeeWage("Reliance", 10, 22, 150);
+            EmpWageBuilderObject DMart = new EmpWageBuilderObject("Dmart",20,20,100);
+            EmpWageBuilderObject Reliance = new EmpWageBuilderObject("Reliance", 10, 22, 150);
+            DMart.computeEmployeeWage();
+            Console.WriteLine(DMart.toString());
+            Reliance.computeEmployeeWage();
+            Console.WriteLine(Reliance.toString());
         }
     }
 }
